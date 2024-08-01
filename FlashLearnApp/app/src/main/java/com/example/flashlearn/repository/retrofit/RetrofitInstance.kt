3dc4c4ec96322ff85dbb37a65da293a22e6874d1
Karen Barcellos <karenkfbarcelos@gmail.com>
2024-08-01 08:52:43 -0300

    Ip

--- FlashLearnApp/app/src/main/java/com/example/flashlearn/repository/retrofit/RetrofitInstance.kt
+++ FlashLearnApp/app/src/main/java/com/example/flashlearn/repository/retrofit/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://192.168.15.94:8080"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     val api: BackendInterface by lazy {
         val retrofit = Retrofit.Builder()
